039c7c8399ed1f92c81aea631a126267eeab3e70
Nguyen Truong <truongnguyen5395@gmail.com>
2021-09-10 12:09:06 +0700

    add show all dot file config

--- config.lua
+++ config.lua
@@ -30,8 +30,11 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
+
+-- NVim tree
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {}
